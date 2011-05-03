c4165321e382de64d36d7db724d2f7b5406955f8
dacruz@gmail.com <dacruz@gmail.com@5de11ed8-a320-4891-0a85-93381678202b>
2011-05-03 00:05:05 +0000

    importando o projeto

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LiveTeacher::Application.config.secret_token = '41ca95ffe9dda0f5551fe4ddf11071d159395c52578a302295acfeeb03402b239b19378aa9faf07492936b296ba59a56744a65f9db4473e2ca07ebece45da9f3'
