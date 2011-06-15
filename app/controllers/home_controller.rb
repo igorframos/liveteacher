b948a2c41b396549d9556905007f77c79716bc8a
igor.bread <igor.bread@5de11ed8-a320-4891-0a85-93381678202b>
2011-06-15 11:41:12 +0000

    O que está no heroku

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,6 @@
 class HomeController < ApplicationController
   def index
+      render :text => "This is the page of the LiveTeacher project.<br><br>Project for the Agile Development discipline at UFRJ."
   end
 
 end
