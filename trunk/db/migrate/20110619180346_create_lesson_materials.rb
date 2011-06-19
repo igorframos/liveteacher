6fc8e0bd77720d6d82a0136c107b3bd04aaae95d
dacruz <dacruz@5de11ed8-a320-4891-0a85-93381678202b>
2011-06-19 19:01:24 +0000

    MVP em 15 minutos! rs...

---
+++ trunk/db/migrate/20110619180346_create_lesson_materials.rb
@@ -0,0 +1,14 @@
+class CreateLessonMaterials < ActiveRecord::Migration
+  def self.up
+    create_table :lesson_materials do |t|
+      t.string :title
+      t.string :file_name
+
+      t.timestamps
+    end
+  end
+
+  def self.down
+    drop_table :lesson_materials
+  end
+end
