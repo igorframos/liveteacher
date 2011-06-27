9925efc09f5cc2e0ed51eee8f34e0de3e645788d
igor.bread <igor.bread@5de11ed8-a320-4891-0a85-93381678202b>
2011-06-27 05:08:44 +0000

    Adicionei mais um teste

--- spec/controllers/lesson_material_controller_spec.rb
+++ spec/controllers/lesson_material_controller_spec.rb
@@ -13,14 +13,31 @@ describe LessonMaterialController do
         flash[:notice].should eql('Choose a title to your upload')
     end
 
-    it 'should save a file correctly' do
+    it 'should save a file correctly and display a message' do
         mock_file = mock(File)
         mock_file.stub!(:original_filename).and_return('blah.txt')
         mock_file.stub!(:read).and_return(65)
-        post 'uploadFile', {:title => 'Teste', :upload =>mock_file}
+        post 'uploadFile', {:title => 'Teste', :upload => mock_file}
         flash[:notice].should eql('Lesson added')
         File.open('public/data/blah.txt', "rb") { |f| f.read.should eql("65") }
         File.delete('public/data/blah.txt')
+    end
+
+    it 'should find all the files uploaded' do
+        mock_file = mock(File)
+        mock_file.stub!(:original_filename).and_return('a.txt', 'b.txt', 'c.txt')
+        mock_file.stub!(:read).and_return(10, 20, 30)
+
+        post 'uploadFile', {:title => 'arquivo 1', :upload => mock_file}
+        post 'uploadFile', {:title => 'arquivo 2', :upload => mock_file}
+        post 'uploadFile', {:title => 'arquivo 3', :upload => mock_file}
+
+        @files = LessonMaterial.find :all
+        @files.size.should eql(3)
+
+        File.delete('public/data/a.txt')
+        File.delete('public/data/b.txt')
+        File.delete('public/data/c.txt')
     end
 
 end
