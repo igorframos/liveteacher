aea680f231e493bc2ec562ec1ed4747d30389028
dacruz <dacruz@5de11ed8-a320-4891-0a85-93381678202b>
2011-06-27 11:38:01 +0000

    refatorando o teste

--- trunk/spec/controllers/lesson_material_controller_spec.rb
+++ trunk/spec/controllers/lesson_material_controller_spec.rb
@@ -2,25 +2,33 @@ require 'spec_helper'
 
 describe LessonMaterialController do
 
+    before :each do
+        @mock_file = mock(File)
+        @mock_file.stub!(:original_filename).and_return('blah.txt')
+        @mock_file.stub!(:read).and_return(65)
+    end
+
+    after :each do
+        begin
+          File.delete('public/data/blah.txt')
+        rescue
+        end
+    end    
+
     it 'should flash an error message when there is no file to upload' do
         post 'uploadFile', {:title => 'Teste'}
         flash[:notice].should eql('Choose a file to upload')
     end
 
     it 'should flash an error message when there is no title to the upload' do
-        mock_file = mock(File)
-        post 'uploadFile', {:title => '', :upload => mock_file}
+        post 'uploadFile', {:title => '', :upload => @mock_file}
         flash[:notice].should eql('Choose a title to your upload')
     end
 
     it 'should save a file correctly and display a message' do
-        mock_file = mock(File)
-        mock_file.stub!(:original_filename).and_return('blah.txt')
-        mock_file.stub!(:read).and_return(65)
-        post 'uploadFile', {:title => 'Teste', :upload => mock_file}
+        post 'uploadFile', {:title => 'Teste', :upload => @mock_file}
         flash[:notice].should eql('Lesson added')
         File.open('public/data/blah.txt', "rb") { |f| f.read.should eql("65") }
-        File.delete('public/data/blah.txt')
     end
 
     it 'should find all the files uploaded' do
