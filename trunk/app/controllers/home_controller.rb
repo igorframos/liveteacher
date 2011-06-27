a99eb8eed5aef98532ab2dc9e1ddde5ce746dc01
igor.bread <igor.bread@5de11ed8-a320-4891-0a85-93381678202b>
2011-06-27 22:48:48 +0000

    Teste final do rcov, espero.

--- trunk/app/controllers/home_controller.rb
+++
@@ -1,6 +0,0 @@
-class HomeController < ApplicationController
-  def index
-      render :text => "This is the page of the LiveTeacher project.<br><br>Project for the Agile Development discipline at UFRJ."
-  end
-
-end
