0e8e3074354e11a544b3b45a692dc41397657dfc
igor.bread <igor.bread@5de11ed8-a320-4891-0a85-93381678202b>
2011-07-02 23:57:22 +0000

    Comecei a criar a página de busca. Vou terminar depois.

---
+++ app/controllers/search_controller.rb
@@ -0,0 +1,15 @@
+class SearchController < ApplicationController
+   
+   skip_before_filter :verify_authenticity_token
+
+  def index
+      render :file => 'app/views/search/search_home.rhtml'
+  end
+
+  def searchByTitle
+      @query = params[:query]
+      @files = LessonMaterial.where("title LIKE '%#{@query}%'")
+      render :file => 'app/views/search/search_results.rhtml'
+  end
+
+end
