2339aa44aedfea8aa45cbf966455dc185e94fce4
igor.bread <igor.bread@5de11ed8-a320-4891-0a85-93381678202b>
2011-07-08 00:29:24 +0000

    Teste novo

---
+++ spec/controllers/search_controller_spec.rb
@@ -0,0 +1,27 @@
+require 'spec_helper'
+
+describe SearchController do
+
+#    before :each do
+#        LessonMaterial.create!({ :title => 'Search test', :file_name => '1.txt'}).save!
+#        LessonMaterial.create!({ :title => 'Testing search', :file_name => '2.txt'}).save!
+#        LessonMaterial.create!({ :title => 'Not a hit', :file_name => '3.txt'}).save!
+#        LessonMaterial.create!({ :title => 'Also not a hit', :file_name => '4.txt'}).save!
+#    end
+
+    it 'should load the results page when searching by title' do
+        post 'searchByTitle', {:query => 'test'}
+        response.should be_success
+    end
+
+    it 'should load the results page when searching by discipline' do
+        post 'searchByDiscipline', {:queryD => 'MAT'}
+        response.should be_success
+    end
+
+#    it 'should find some results' do
+#        post 'searchByTitle', { :query => 'test' }
+#        response.should be_success
+#    end
+
+end
