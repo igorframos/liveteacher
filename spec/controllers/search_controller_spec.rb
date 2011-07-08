0efc4ea2cc9b0600a8b87802575925701793e495
igor.bread <igor.bread@5de11ed8-a320-4891-0a85-93381678202b>
2011-07-08 03:24:38 +0000

    Atualizações em alguns testes e na busca por título. Tentei colocar o webrat para funcionar, mas não estou conseguindo fazer testes que usem a interface.

--- spec/controllers/search_controller_spec.rb
+++ spec/controllers/search_controller_spec.rb
@@ -2,12 +2,12 @@ require 'spec_helper'
 
 describe SearchController do
 
-#    before :each do
-#        LessonMaterial.create!({ :title => 'Search test', :file_name => '1.txt'}).save!
-#        LessonMaterial.create!({ :title => 'Testing search', :file_name => '2.txt'}).save!
-#        LessonMaterial.create!({ :title => 'Not a hit', :file_name => '3.txt'}).save!
-#        LessonMaterial.create!({ :title => 'Also not a hit', :file_name => '4.txt'}).save!
-#    end
+    before :each do
+        LessonMaterial.create!({ :title => 'Search test', :file_name => '1.txt'}).save!
+        LessonMaterial.create!({ :title => 'Testing search', :file_name => '2.txt'}).save!
+        LessonMaterial.create!({ :title => 'Not a hit', :file_name => '3.txt'}).save!
+        LessonMaterial.create!({ :title => 'Also not a hit', :file_name => '4.txt'}).save!
+    end
 
     it 'should load the results page when searching by title' do
         post 'searchByTitle', {:query => 'test'}
@@ -19,9 +19,14 @@ describe SearchController do
         response.should be_success
     end
 
-#    it 'should find some results' do
-#        post 'searchByTitle', { :query => 'test' }
-#        response.should be_success
-#    end
+    it 'should render the correct page when searching by title' do
+        post 'searchByTitle', { :query => 'test' }
+        response.should render_template('search_results')
+    end
+
+    it 'should render the correct page when searching by discipline' do
+        post 'searchByDiscipline', { :queryD => 'MAT' }
+        response.should render_template('search_results')
+    end
 
 end
