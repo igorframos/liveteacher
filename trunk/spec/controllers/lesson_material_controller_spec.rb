8a0964ce4cddfaba3901401f0aee0c8190fec9a9
dacruz <dacruz@5de11ed8-a320-4891-0a85-93381678202b>
2011-07-12 21:59:33 +0000

    colocando campo de diciplana na tela e fazendo ele chegar no banco

--- trunk/spec/controllers/lesson_material_controller_spec.rb
+++ trunk/spec/controllers/lesson_material_controller_spec.rb
@@ -17,36 +17,48 @@ describe LessonMaterialController do
         end
     end    
 
+
     it 'should flash an error message when there is no file to upload' do
-        post 'uploadFile', {:title => 'Teste'}
+        post 'uploadFile', {:title => 'Teste', :discipline => 'MAT'}
         flash[:notice].should eql('Choose a file to upload')
     end
 
     it 'should flash an error message when there is no title to the upload' do
-        post 'uploadFile', {:title => '', :upload => @mock_file}
+        post 'uploadFile', {:title => '', :upload => @mock_file, :discipline => 'MAT'}
         flash[:notice].should eql('Choose a title to your upload')
     end
 
+    it 'should flash an error message when there is no discipline to the upload' do
+        post 'uploadFile', {:title => 'Teste', :upload => @mock_file}
+        flash[:notice].should eql('Choose a discipline to your upload')
+    end
+
+    it 'should flash an error message when discipline is EMPTY' do
+        post 'uploadFile', {:title => 'Teste', :upload => @mock_file, :discipline => 'EMPTY'}
+        flash[:notice].should eql('Choose a discipline to your upload')
+    end
+
+
     it 'should load the page correctly when an upload is requested' do
-        post 'uploadFile', { :title => 'Teste', :upload => @mock_file }
+        post 'uploadFile', { :title => 'Teste', :upload => @mock_file, :discipline => 'MAT' }
         response.should be_success
     end
 
     it 'should render the correct page when an upload is made' do
-        post 'uploadFile', { :title => 'Teste', :upload => @mock_file }
+        post 'uploadFile', { :title => 'Teste', :upload => @mock_file, :discipline => 'MAT' }
         response.should render_template('index')
     end
 
     it 'should save a file correctly and display a message' do
-        post 'uploadFile', {:title => 'Teste', :upload => @mock_file}
+        post 'uploadFile', {:title => 'Teste', :upload => @mock_file, :discipline => 'MAT'}
         flash[:notice].should eql('Lesson added')
         File.open('public/data/blah.txt', "rb") { |f| f.read.should eql("65") }
     end
 
     it 'should find all the files uploaded' do
-        post 'uploadFile', {:title => 'arquivo 1', :upload => @mock_file}
-        post 'uploadFile', {:title => 'arquivo 2', :upload => @mock_file}
-        post 'uploadFile', {:title => 'arquivo 3', :upload => @mock_file}
+        post 'uploadFile', {:title => 'arquivo 1', :upload => @mock_file, :discipline => 'MAT'}
+        post 'uploadFile', {:title => 'arquivo 2', :upload => @mock_file, :discipline => 'MAT'}
+        post 'uploadFile', {:title => 'arquivo 3', :upload => @mock_file, :discipline => 'MAT'}
 
         @files = LessonMaterial.find :all
         @files.size.should eql(3)
