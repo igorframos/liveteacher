8b8f1d51798c962b4c604f5158163b85306d4d43
igor.bread <igor.bread@5de11ed8-a320-4891-0a85-93381678202b>
2011-07-15 04:30:03 +0000

    Adicionei alguns testes de aceitação que usam a interface. Amanhã vou tentar lembrar se esqueci algum, mas já dei uma adiantada na pior das hipóteses.

--- spec/controllers/lesson_material_controller_spec.rb
+++ spec/controllers/lesson_material_controller_spec.rb
@@ -2,11 +2,13 @@ require 'spec_helper'
 
 describe LessonMaterialController do
 
+    render_views
+
     before :each do
         @mock_file = mock(File)
         @mock_file.stub!(:original_filename).and_return('blah.txt', 'a.txt', 'b.txt')
         @mock_file.stub!(:read).and_return(65, 10, 20)
-        @material = LessonMaterial.create!(:title => 'arquivo 1', :file_name => '1.txt', :discipline => 'MAT')
+        @material = LessonMaterial.create!(:title => 'arquivo 4', :file_name => '1.txt', :discipline => 'MAT')
     end
 
     after :each do
@@ -66,6 +68,16 @@ describe LessonMaterialController do
         @files.size.should eql(4)
     end
 
+    it 'should show the all the files correctly' do
+        post 'uploadFile', {:title => 'arquivo 1', :upload => @mock_file, :discipline => 'MAT'}
+        post 'uploadFile', {:title => 'arquivo 2', :upload => @mock_file, :discipline => 'MAT'}
+        post 'uploadFile', {:title => 'arquivo 3', :upload => @mock_file, :discipline => 'MAT'}
+
+        response.should contain('arquivo 1')
+        response.should contain('arquivo 2')
+        response.should contain('arquivo 3')
+    end
+
     it 'should find the correct file when asked for its details' do
         get 'details', {:id => @material.id}
         assigns[:material].id.should eql(@material.id)
@@ -111,6 +123,19 @@ describe LessonMaterialController do
 
       @comments = Comments.find :all
       @comments.size.should eql(0)
+    end
+
+    it 'should show the comment posted' do
+      post 'comment', {:id => @material.id, :name => 'Author', :email => 'tester@liveteacher.com', :text => 'Nice'}
+
+      response.should contain('Author')
+      response.should contain('Nice')
+    end
+
+    it 'should not show a person\'s email' do
+      post 'comment', {:id => @material.id, :name => 'Author', :email => 'tester@liveteacher.com', :text => 'Nice'}
+
+      response.should_not contain('tester@liveteacher.com')
     end
 
 end
--- spec/controllers/search_controller_spec.rb
+++ spec/controllers/search_controller_spec.rb
@@ -2,11 +2,13 @@ require 'spec_helper'
 
 describe SearchController do
 
+    render_views
+
     before :each do
-        LessonMaterial.create!({ :title => 'Search test', :file_name => '1.txt'}).save!
-        LessonMaterial.create!({ :title => 'Testing search', :file_name => '2.txt'}).save!
-        LessonMaterial.create!({ :title => 'Not a hit', :file_name => '3.txt'}).save!
-        LessonMaterial.create!({ :title => 'Also not a hit', :file_name => '4.txt'}).save!
+        LessonMaterial.create!({ :title => 'Search test', :file_name => '1.txt', :discipline => 'MAT'}).save!
+        LessonMaterial.create!({ :title => 'Testing search', :file_name => '2.txt', :discipline => 'MAT'}).save!
+        LessonMaterial.create!({ :title => 'Not a hit', :file_name => '3.txt', :discipline => 'POR'}).save!
+        LessonMaterial.create!({ :title => 'Also not a hit', :file_name => '4.txt', :discipline => 'POR'}).save!
     end
 
     it 'should load the results page when searching by title' do
@@ -27,6 +29,42 @@ describe SearchController do
     it 'should render the correct page when searching by discipline' do
         post 'searchByDiscipline', { :queryD => 'MAT' }
         response.should render_template('search_results')
+    end
+
+    it 'should find the correct files when searching by title' do
+        post 'searchByTitle', { :query => 'test' }
+        response.should contain('Search test')
+        response.should contain('Testing search')
+    end
+
+    it 'should not find wrong files when searching by title' do
+        post 'searchByTitle', { :query => 'test' }
+        response.should_not contain('Not a hit')
+        response.should_not contain('Also not a hit')
+    end
+
+    it 'should find the correct files when searching by title with more than one word' do
+        post 'searchByTitle', { :query => 'test search' }
+        response.should contain('Search test')
+        response.should contain('Testing search')
+    end
+
+    it 'should not find wrong files when searching by title with more than one word' do
+        post 'searchByTitle', { :query => 'test search' }
+        response.should_not contain('Not a hit')
+        response.should_not contain('Also not a hit')
+    end
+
+    it 'should find the correct files when searching by discipline' do
+        post 'searchByDiscipline', { :queryD => 'MAT' }
+        response.should contain('Search test')
+        response.should contain('Testing search')
+    end
+
+    it 'should not find the wrong files when searching by discipline' do
+        post 'searchByDiscipline', { :queryD => 'MAT' }
+        response.should_not contain('Not a hit')
+        response.should_not contain('Also not a hit')
     end
 
 end
