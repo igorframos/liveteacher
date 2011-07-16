542af814a8eb770945c404e1e89ccc30eba6bc53
igor.bread <igor.bread@5de11ed8-a320-4891-0a85-93381678202b>
2011-07-16 04:54:09 +0000

    Adicionei mais alguns testes. A maioria é teste de aceitação.

--- trunk/app/controllers/lesson_material_controller.rb
+++ trunk/app/controllers/lesson_material_controller.rb
@@ -5,6 +5,7 @@ class LessonMaterialController < ApplicationController
   def index
     @files = LessonMaterial.find :all
     render :file => 'app/views/lesson_material/index.rhtml'
+    flash[:notice] = ''
   end
 
   def uploadFile
@@ -35,26 +36,31 @@ class LessonMaterialController < ApplicationController
       @material = LessonMaterial.find_by_id params[:id]
       @comments = Comments.find_all_by_lesson_material_id @material.id
       render :file => 'app/views/lesson_material/details.rhtml'
+      flash[:notice] = ''
   end
 
   def comment
-      unless params[:id] and params[:id].to_i >= 0
+      unless params[:id] and params[:id].to_i >= 0 and params[:id].to_i <= LessonMaterial.count
           params[:id] = 1
+          flash[:notice] = 'Something wrong has happened. We\'re allready investigating it'
           self.details
           return
       end
 
       unless params[:name] and params[:name] != ''
+          flash[:notice] = 'Please, enter the author name to comment'
           self.details
           return
       end
 
-      unless params[:email] and params[:email] != ''
+      unless params[:email] and params[:email] != '' and params[:email] =~ /((\d|\w|\.)+)@(\d|\w)+\.(\d|\w|\.)+/
+          flash[:notice] = 'Please, enter a valid email address'
           self.details
           return
       end
 
       unless params[:text] and params[:text] != ''
+          flash[:notice] = 'There must be some text on your comment'
           self.details
           return
       end
@@ -66,6 +72,7 @@ class LessonMaterialController < ApplicationController
 
       Comments.save(materialId, name, email, text)
 
+      flash[:notice] = 'Comment successfully added!'
       self.details
   end
 
